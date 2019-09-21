07b6e7a0e46622a21d579829537864a96addefbc
Steven Ding <steven.h.ding@mail.mcgill.ca>
2019-09-21 10:57:20 -0400

    Signed-off-by: Steven Ding <steven.h.ding@mail.mcgill.ca>

---
+++ java-project/src/main/java/ca/queensu/cisc327/App.java
@@ -0,0 +1,13 @@
+package ca.queensu.cisc327;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
