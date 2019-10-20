524c9480bfb89aa39fa40a419a159bebd4d34011
Steven Ding <steven.h.ding@mail.mcgill.ca>
2019-10-19 21:10:21 -0400

    Signed-off-by: Steven Ding <steven.h.ding@mail.mcgill.ca>

--- src/main/java/ca/queensu/cisc327/App.java
+++ src/main/java/ca/queensu/cisc327/App.java
@@ -1,13 +1,37 @@
 package ca.queensu.cisc327;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    /***
+     * An example program that has two functionalities:
+        R1/ Ask for user's name and print it out
+        R2/ Write 'hello 327' to the file specified by app
+            argument if the user's name is 327.
+     */
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello world!" );
+
+        // R1:
+        System.out.println("What is your name please?:");
+        Scanner scanner = new Scanner(System.in);
+        String name =scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        // R2:
+        if(name.equals("327")){
+            Files.write(new File(args[0]).toPath(), "hello 327".getBytes());
+            System.out.println("file written!");
+        }
+        scanner.close();
     }
 }
--- src/test/java/ca/queensu/cisc327/AppTest.java
+++ src/test/java/ca/queensu/cisc327/AppTest.java
@@ -1,38 +1,88 @@
 package ca.queensu.cisc327;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.*;
+
+import ca.queensu.cisc327.App;
+
+public class AppTest {
+
+    @Test
+    public void testAppR1() throws Exception {
+        runAndTest(Arrays.asList("steven"), Arrays.asList("Hello steven"), null);
+
+    }
+
+    @Test
+    public void testAppR2() throws Exception {
+        runAndTest(Arrays.asList("327"), Arrays.asList("Hello 327", "file written!"),
+                getFileFromResource("/R2/expected.txt"));
+
     }
 
     /**
-     * @return the suite of tests being tested
+     * Helper function to run the main function and verify the output
+     * @param terminal_input A list of string as the terminal input to run the program
+     * @param expected_terminal_tails A list of string expected at the tail of terminal output
+     * @param expected_output_file A file that contains the expected content for the output file
+     * @throws Exception
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public void runAndTest(List<String> terminal_input, List<String> expected_terminal_tails,
+            String expected_output_file) throws Exception {
+
+        // setup parameters for the program to run
+        // create a temporary file
+        File tmpFile = File.createTempFile("temp-test", ".tmp");
+        String[] args = { tmpFile.getAbsolutePath() };
+
+        // setup user input
+        String userInput = String.join(System.lineSeparator(), terminal_input);
+        ByteArrayInputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+
+        // setup stdin & stdout:
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+
+        // run the program
+        App.main(args);
+
+        // capture terminal outputs:
+        String[] printed_lines = outContent.toString().split("[\r\n]+");
+
+        // compare the tail of the terminal outputs:
+        int diff = printed_lines.length - expected_terminal_tails.size();
+        for (int i = 0; i < expected_terminal_tails.size(); ++i) {
+            assertEquals(expected_terminal_tails.get(i), printed_lines[i + diff]);
+        }
+
+        // compare output file content to the expected content
+        if (expected_output_file != null) {
+            String expected_output = new String(Files.readAllBytes(Paths.get(expected_output_file)), "UTF-8");
+            String actual_output = new String(Files.readAllBytes(tmpFile.toPath()), "UTF-8");
+            assertEquals(expected_output, actual_output);
+        }
+
     }
 
     /**
-     * Rigourous Test :-)
+     * Retrieve the absolute path of the files in the resources folder
+     * @param relativePath The file's relative path in the resources folder (/test/resources)
+     * @return the absolute path of the file in the resource folder. 
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    String getFileFromResource(String relativePath) {
+        return new File(this.getClass().getResource(relativePath).getFile()).getAbsolutePath();
     }
 }
